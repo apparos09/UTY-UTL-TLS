7150e42fee8e3eebffb889cb0a24eb7f994e6555
mecha-rm <roderick.montague243@gmail.com>
2023-04-14 22:57:50 -0400

    Added Rotation Calculations
    Added calculations for rotating a vector.

---
+++ Assets/Scripts/SampleManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using util;
+
+// Manages the sample scene.
+public class SampleManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 v = new Vector3(1, 0, 0);
+        float theta = 45.0F;
+
+        Debug.Log("Rotating " + v.ToString() + " by " + theta.ToString() + " Degrees (2D) | " +
+            CustomMath.Rotate(new Vector2(v.x, v.y), theta, true).ToString());
+
+        Debug.Log("Rotating " + v.ToString() + " by " + theta.ToString() + " Degrees (X-3D) | " +
+            CustomMath.RotateX(v, theta, true).ToString());
+
+        Debug.Log("Rotating " + v.ToString() + " by " + theta.ToString() + " Degrees (Y-3D) | " +
+            CustomMath.RotateY(v, theta, true).ToString());
+
+        Debug.Log("Rotating " + v.ToString() + " by " + theta.ToString() + " Degrees (Z-3D) | " +
+            CustomMath.RotateZ(v, theta, true).ToString());        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
