0cb205905baa85640e5b5f6e9be52c4afd275a91
mecha-rm <roderick.montague243@gmail.com>
2023-07-13 22:55:54 -0400

    Renamed the AudioSourceLooper Script
    Renamed the audio source looper script (originally called the audio segment looper script).

--- Assets/Scripts/AudioSceneManager.cs
+++ Assets/Scripts/AudioSceneManager.cs
@@ -34,7 +34,7 @@ public class AudioSceneManager : MonoBehaviour
     public AudioFader audioFader;
 
     // The audio looper.
-    public AudioSegmentLooper looper;
+    public AudioSourceLooper looper;
 
     // Start is called before the first frame update
     void Start()
