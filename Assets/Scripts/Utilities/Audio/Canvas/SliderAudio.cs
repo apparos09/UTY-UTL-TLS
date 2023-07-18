2127e028dfb3999489370c87afcf1d0435729636
Roderick Montague <roderick.montague243@gmail.com>
2023-07-17 23:16:30 -0400

    Added Slider Audio
    Added a script for adding audio to a slider component in Unity.

--- Assets/Scripts/Utilities/Audio/Canvas/SliderAudio.cs
+++ Assets/Scripts/Utilities/Audio/Canvas/SliderAudio.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 
 namespace util
 {
-    // Plays audio when clicking a toggle.
-    public class ToggleAudio : MonoBehaviour
+    // Adds audio to a slider.
+    public class SliderAudio : MonoBehaviour
     {
-        // The button this script is for.
-        public Toggle toggle;
+        // The slider this script is for.
+        public Slider slider;
 
         // THe audio for the user inputs.
         public AudioSource audioSource;
@@ -20,13 +20,13 @@ namespace util
         // Awake is called when the script instance is being loaded.
         private void Awake()
         {
-            // Moved here in case the toggle has not been set enabled before the game was closed.
+            // Moved here in case the slider has not been set enabled before the game was closed.
 
             // Button not set.
-            if (toggle == null)
+            if (slider == null)
             {
                 // Tries to get the component.
-                toggle = GetComponent<Toggle>();
+                slider = GetComponent<Slider>();
             }
 
         }
@@ -34,34 +34,34 @@ namespace util
         // Add OnValueChanged Delegate
         public void AddOnValueChanged()
         {
-            // If the toggle isn't set, return.
-            if (toggle == null)
+            // If the slider isn't set, return.
+            if (slider == null)
                 return;
 
             // Listener for the tutorial toggle.
-            toggle.onValueChanged.AddListener(delegate
+            slider.onValueChanged.AddListener(delegate
             {
-                OnValueChanged(toggle.isOn);
+                OnValueChanged(slider.value);
             });
         }
 
         // Remove OnValueChanged Delegate
         public void RemoveOnValueChanged()
         {
-            // If the toggle isn't set, return.
-            if (toggle == null)
+            // If the slider isn't set, return.
+            if (slider == null)
                 return;
 
-            // Remove the listener for onValueChanged if the toggle has been set.
-            if (toggle != null)
+            // Remove the listener for onValueChanged if the slider has been set.
+            if (slider != null)
             {
-                toggle.onValueChanged.RemoveListener(OnValueChanged);
+                slider.onValueChanged.RemoveListener(OnValueChanged);
             }
         }
 
 
-        // Called when the toggle is clicked.
-        private void OnValueChanged(bool isOn)
+        // Called when the slider has been changed.
+        private void OnValueChanged(float value)
         {
             if (audioSource != null && audioClip != null)
                 audioSource.PlayOneShot(audioClip);
