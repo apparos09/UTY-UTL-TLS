f28e73eb64dd1d9d335b64824ca17d025ce2b269
Roderick Montague <roderick.montague243@gmail.com>
2024-05-10 15:05:49 -0400

    Added Game Settings Base
    Added a base game settings file that can be adapted into other projects.

---
+++ Assets/Scripts/Utilities/GameSettings.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace util
+{
+    // The game settings.
+    public class GameSettings : MonoBehaviour
+    {
+        // The game settings instance.
+        private static GameSettings instance;
+
+        // Gets set to 'true' when the singleton is initialized.
+        private bool initialized = false;
+
+        // If 'true', tutorial elements are used.
+        public bool useTutorial = true;
+
+        // Constructor
+        private GameSettings()
+        {
+            // ...
+
+        }
+
+        // Awake is called when the script is being loaded
+        void Awake()
+        {
+            // If the instance hasn't been set, set it to this object.
+            if (instance == null)
+            {
+                instance = this;
+            }
+            // If the instance isn't this, destroy the game object.
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            // Run code for initialization.
+            if (!initialized)
+            {
+                initialized = true;
+            }
+        }
+
+        // Start is called just before any of the Update methods is called the first time
+        private void Start()
+        {
+            // Don't destroy this object on load.
+            DontDestroyOnLoad(this);
+        }
+
+        // Gets the instance.
+        public static GameSettings Instance
+        {
+            get
+            {
+                // Checks if the instance exists.
+                if (instance == null)
+                {
+                    // Tries to find the instance.
+                    instance = FindObjectOfType<GameSettings>(true);
+
+
+                    // The instance doesn't already exist.
+                    if (instance == null)
+                    {
+                        // Generate the instance.
+                        GameObject go = new GameObject("Game Settings (singleton)");
+                        instance = go.AddComponent<GameSettings>();
+                    }
+
+                }
+
+                // Return the instance.
+                return instance;
+            }
+        }
+
+        // Returns 'true' if the object has been initialized.
+        public bool Initialized
+        {
+            get
+            {
+                return initialized;
+            }
+        }
+
+        // Quits the application.
+        public static void QuitApplication()
+        {
+            Application.Quit();
+        }
+    }
+}
