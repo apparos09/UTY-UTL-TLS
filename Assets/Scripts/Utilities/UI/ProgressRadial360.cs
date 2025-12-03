23f8e4f4cb2da9be2c2a6e943d2099c6d332810c
mecha-rm <roderick.montague243@gmail.com>
2025-12-03 00:42:09 -0500

    Added Updates from Main Branch
    The updates from the main branch have been added, and builds were generated.

---
+++ Assets/Scripts/Utilities/UI/ProgressRadial360.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+namespace util
+{
+    // A script for a progress radial 360.
+    // Build off of the ProgessBar script.
+    public class ProgressRadial360 : ProgressBar
+    {
+        // Since there's no unique functionality in ProgressRadial360 these functions have been commented out...
+        // As these functions don't actually do anything aside from call their inherited functions.
+
+        // // Awake is called when the script instance is being loaded
+        // protected override void Awake()
+        // {
+        //     base.Awake();
+        // }
+        // 
+        // // Start is called once before the first execution of Update after the MonoBehaviour is created
+        // protected override void Start()
+        // {
+        //     base.Start();
+        // }
+        // 
+        // // Update is called once per frame
+        // protected override void Update()
+        // {
+        //     base.Update();
+        // }
+    }
+}
