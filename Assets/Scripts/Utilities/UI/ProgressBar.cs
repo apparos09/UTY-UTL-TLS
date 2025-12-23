0a6e0dee306cad8dbdf6d3d27dff50c1d2827bfc
mecha-rm <roderick.montague243@gmail.com>
2025-12-22 23:37:01 -0500

    Updated Progress Bar Script
    Updated the progress bar script to add callbacks, to fix the min and max values, and to have the bar value set in Awake() to fix a transition animation issue.

--- Assets/Scripts/Utilities/UI/ProgressBar.cs
+++ Assets/Scripts/Utilities/UI/ProgressBar.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using static util.TextBox;
 
 namespace util
 {
@@ -11,14 +12,22 @@ namespace util
         // The slider bar that's being animated.
         public Slider bar;
 
-        // The minimum value.
-        private float minValue = 0.0F;
+        // Checks the bar for the min and max values instead, since that makes more sense.
+        // Doing the min and max values this way also prevented the user from changing the min-max values on the bar.
 
-        // The maximum value.
-        private float maxValue = 1.0F;
+        // // The minimum value.
+        // private float minValue = 0.0F;
+        // 
+        // // The maximum value.
+        // private float maxValue = 1.0F;
 
         // The value for the progress bar.
+        // The progress bar script prevents the value from being set by the Slider script.
         private float value = 0.0F;
+
+        // Sets the progress bar's value to the bar's starting value on awake if true.
+        [Tooltip("Sets the progress value using the slider's starting value in Awake() if true.")]
+        public bool setValueOnAwake = true;
 
         [Header("Speed")]
 
@@ -38,31 +47,60 @@ namespace util
         // Set to 'true' if the bar is transitioning between values.
         private bool transitioning = false;
 
-        // value t for interlation
+        // The value t for interpolation.
         private float v_t = 0.0F;
+
+        // Callbacks
+        // A callback for when the progress bar's transition finishes.
+        // If there is no transition, the start and end functions are called instantly.
+        public delegate void TransitionCallback();
+
+        // Callback for the transition starting.
+        private TransitionCallback transitionStartCallback;
+
+        // Callback for the transtion ending.
+        private TransitionCallback transitionEndCallback;
 
         // Awake is called when the script instance is being loaded
         protected virtual void Awake()
         {
-            // ...
+            // Tries to get the slider component if it isn't set.
+            if (bar == null)
+                bar = GetComponent<Slider>();
+
+            // If the bar isn't null, do starting functions.
+            if(bar != null)
+            {
+                // Sets the value on awake if enabled.
+                // This is done here in case a SetValue() function call happens in the Start() function of another object.
+                // If it was, doing this without the transition would stop the effect if the first call in the object's Start()...
+                // Said to use the transition.
+                if(setValueOnAwake)
+                    SetValueAsPercentage(bar.value / bar.maxValue, false);
+            }
         }
 
         // Start is called before the first frame update
         protected virtual void Start()
         {
-            // Grabs the slider component from the object if this hasn't been set.
+            // Tries to get the slider component a second time if it isn't set.
+            // Originally this only happened in Start, but it's done in Awake as well...
+            // To make sure the bar matches the slider's starting value if possible.
             if (bar == null)
                 bar = GetComponent<Slider>();
         }
 
+        // Getters and setters for the minimum and maximum values.
+        // If the setters are used, the SetValue() function is called to adjust the progress bar.
+
         // The minimum value.
         public float MinValue
         {
-            get { return minValue; }
+            get { return bar.minValue; }
 
             set
             {
-                minValue = value;
+                bar.minValue = value;
                 SetValue(this.value);
             }
         }
@@ -70,11 +108,11 @@ namespace util
         // The maximum value.
         public float MaxValue
         {
-            get { return maxValue; }
+            get { return bar.maxValue; }
 
             set
             {
-                maxValue = value;
+                bar.maxValue = value;
                 SetValue(this.value);
             }
         }
@@ -85,24 +123,26 @@ namespace util
             return value;
         }
 
-        // TODO: account for a transition already in progress.
         // Sets the value for the progress bar.
         public void SetValue(float newValue, bool transition = true)
         {
             // If the minimum value is larger than the maximum value they will be swapped.
-            if (minValue > maxValue)
+            // Since the bar min and max values are being used, this may not be necessary...
+            // But the check has stil been included regardless.
+            if (bar.minValue > bar.maxValue)
             {
-                float temp = minValue;
-                minValue = maxValue;
-                maxValue = temp;
+                float temp = bar.minValue;
+                bar.minValue = bar.maxValue;
+                bar.maxValue = temp;
             }
 
             // Applies the new value.
             startValue = value;
-            value = Mathf.Clamp(newValue, minValue, maxValue);
+            value = Mathf.Clamp(newValue, bar.minValue, bar.maxValue);
 
-            bar.minValue = minValue;
-            bar.maxValue = maxValue;
+            // Not needed since the bar is used directly now.
+            // bar.minValue = minValue;
+            // bar.maxValue = maxValue;
 
             v_t = 0.0F;
 
@@ -115,6 +155,10 @@ namespace util
 
                 // Transitioning.
                 transitioning = true;
+
+                // Callback(s)
+                // Calls OnTransitionStart(). OnTransitionEnd() will be called when the transition finishes.
+                OnTransitionStart();
             }
             else
             {
@@ -124,19 +168,24 @@ namespace util
                 // Changes the progress bar visual.
                 startValue = value;
                 bar.value = value;
+
+                // Callback(s)
+                // Calls start and end since there is no transition.
+                OnTransitionStart();
+                OnTransitionEnd();
             }
         }
 
         // Returns the value in a 0.0 (0%) to 1.0 (100%) range.
         public float GetValueAsPercentage()
         {
-            return Mathf.InverseLerp(minValue, maxValue, value);
+            return Mathf.InverseLerp(bar.minValue, bar.maxValue, value);
         }
 
         // Sets the value as a percentage.
         public void SetValueAsPercentage(float newValue, bool transition = true)
         {
-            SetValue(newValue * maxValue, transition);
+            SetValue(newValue * bar.maxValue, transition);
         }
 
         // Gets the value from the slider UI object.
@@ -157,6 +206,48 @@ namespace util
             return transitioning;
         }
 
+        // Adds a callback for the transition starting.
+        public void OnTransitionStartAddCallback(TransitionCallback callback)
+        {
+            transitionStartCallback += callback;
+        }
+
+        // Removes a callback for the transition starting.
+        public void OnTransitionStartRemoveCallback(TransitionCallback callback)
+        {
+            transitionStartCallback -= callback;
+        }
+
+        // Called when the transition has started.
+        // This is still called even if the transition is instant.
+        private void OnTransitionStart()
+        {
+            // Checks if there are functions to call.
+            if (transitionStartCallback != null)
+                transitionStartCallback();
+        }
+
+        // Adds a callback for the transition ending.
+        public void OnTransitionEndAddCallback(TransitionCallback callback)
+        {
+            transitionEndCallback += callback;
+        }
+
+        // Removes a callback for the transition ending.
+        public void OnTransitionEndRemoveCallback(TransitionCallback callback)
+        {
+            transitionEndCallback -= callback;
+        }
+
+        // Called when the transition has ended.
+        // This is still called even if the transition is instant.
+        private void OnTransitionEnd()
+        {
+            // Checks if there are functions to call.
+            if (transitionEndCallback != null)
+                transitionEndCallback();
+        }
+
         // Update is called once per frame
         protected virtual void Update()
         {
@@ -173,7 +264,7 @@ namespace util
                     // If the start value is less than the destination value then the bar is increasing.
                     if (value > startValue) // Increase
                     {
-                        bar.value = Mathf.Lerp(minValue, maxValue, v_t);
+                        bar.value = Mathf.Lerp(bar.minValue, bar.maxValue, v_t);
 
                         // If the bar value has reached the desired value then it should stop moving.
                         if (bar.value >= value)
@@ -185,7 +276,7 @@ namespace util
                     }
                     else // Decrease
                     {
-                        bar.value = Mathf.Lerp(maxValue, minValue, v_t);
+                        bar.value = Mathf.Lerp(bar.maxValue, bar.minValue, v_t);
 
                         // If the bar value has reached the desired value then it should stop moving.
                         if (bar.value <= value)
@@ -206,6 +297,9 @@ namespace util
                 {
                     v_t = 0.0F;
                     transitioning = false;
+
+                    // Callback
+                    OnTransitionEnd();
                 }
 
             }
@@ -214,8 +308,11 @@ namespace util
                 // if the bar value does not match.
                 if (bar.value != value)
                 {
-                    bar.minValue = minValue;
-                    bar.maxValue = maxValue;
+                    // Sets the values.
+                    // Not needed since the bar is checked for the min and max directly.
+                    // bar.minValue = minValue;
+                    // bar.maxValue = maxValue;
+
                     bar.value = value;
                 }
             }
